404855cbed59043b18aeeae9da742b59941b691a
heyzec <61238538+heyzec@users.noreply.github.com>
2024-10-03 00:08:02 +0800

    terraform: update server path endpoint and clean up proxy.tf

--- infra/proxy.tf
+++ infra/proxy.tf
@@ -1,4 +1,4 @@
-# Create a Google-managed SSL certificate for the Load Balancer
+# Google-managed SSL certificate
 resource "google_compute_managed_ssl_certificate" "default" {
   name = "binacloud-ssl-cert"
   
@@ -10,9 +10,84 @@ resource "google_compute_managed_ssl_certificate" "default" {
   }
 }
 
-# Create an unmanaged instance group and add your existing Compute Engine instance
-resource "google_compute_instance_group" "default" {
-  name = "minimal-instance-group"
+# ================================================================================
+# 1. Global forwarding rule to listen on port 443 (HTTPS)
+# ================================================================================
+resource "google_compute_global_forwarding_rule" "default" {
+  name       = "default"
+  target     = google_compute_target_https_proxy.default.self_link
+  port_range = "443"
+  load_balancing_scheme = "EXTERNAL"
+}
+
+# ================================================================================
+# 2. HTTPS proxy for SSL termination
+# ================================================================================
+resource "google_compute_target_https_proxy" "default" {
+  name             = "default"
+  url_map          = google_compute_url_map.default.self_link
+  ssl_certificates = [google_compute_managed_ssl_certificate.default.self_link]
+}
+
+# ================================================================================
+# 3. Route (unencrypted) traffic to respective backend service based on hosts and path
+# ================================================================================
+resource "google_compute_url_map" "default" {
+  name            = "default"
+  default_service = google_compute_backend_service.client-service.self_link
+
+  host_rule {
+    hosts        = ["*"]
+    path_matcher = "default"
+  }
+
+  path_matcher {
+    name            = "default"
+    default_service = google_compute_backend_service.client-service.self_link
+
+    path_rule {
+      paths   = ["/server/*"]
+      service = google_compute_backend_service.server-service.self_link
+    }
+  }
+}
+
+# ================================================================================
+# 4a. Client (cloud run) backend service
+# ================================================================================
+resource "google_compute_backend_service" "client-service" {
+  name                  = "client-service"
+  protocol              = "HTTP"
+  timeout_sec           = 30
+  backend {
+    group = google_compute_region_network_endpoint_group.client-group.self_link
+  }
+  port_name = "http"
+}
+resource "google_compute_region_network_endpoint_group" "client-group" {
+  name = "client-group"
+  region = "us-central1"
+  cloud_run {
+    service = "huawei-poc"
+  }
+}
+
+
+# ================================================================================
+# 4b. Client (cloud run) backend service
+# ================================================================================
+resource "google_compute_backend_service" "server-service" {
+  name                  = "server-service"
+  protocol              = "HTTP"
+  timeout_sec           = 30
+  backend {
+    group = google_compute_instance_group.server-group.self_link
+  }
+  port_name = "http"
+  health_checks = [google_compute_http_health_check.server-health-check.self_link]
+}
+resource "google_compute_instance_group" "server-group" {
+  name = "server-group"
   instances = [
     google_compute_instance.server.self_link
   ]
@@ -21,47 +96,11 @@ resource "google_compute_instance_group" "default" {
     port = 8000
   }
 }
-
-# Create a backend service that uses the instance group as its backend
-resource "google_compute_backend_service" "default" {
-  name                  = "minimal-backend-service"
-  load_balancing_scheme = "EXTERNAL"
-  protocol              = "HTTP"
-  timeout_sec           = 30
-  backend {
-    group = google_compute_instance_group.default.self_link
-  }
-  # Use the named port defined in the instance group
-  port_name = "http"
-  health_checks = [google_compute_http_health_check.default.self_link]
-}
-
-# Create a URL map to route traffic to the backend service
-resource "google_compute_url_map" "default" {
-  name            = "minimal-url-map"
-  default_service = google_compute_backend_service.default.self_link
-}
-
-# Create a target HTTPS proxy for SSL termination
-resource "google_compute_target_https_proxy" "default" {
-  name             = "minimal-https-proxy"
-  url_map          = google_compute_url_map.default.self_link
-  ssl_certificates = [google_compute_managed_ssl_certificate.default.self_link]
-}
-
-# Create a global forwarding rule to listen on port 443 (HTTPS)
-resource "google_compute_global_forwarding_rule" "default" {
-  name       = "minimal-https-forwarding-rule"
-  target     = google_compute_target_https_proxy.default.self_link
-  port_range = "443"
-  load_balancing_scheme = "EXTERNAL"
-}
-
-# Create a basic HTTP health check to monitor instance health
-resource "google_compute_http_health_check" "default" {
-  name               = "minimal-health-check"
+# Health check is needed for compute instances
+resource "google_compute_http_health_check" "server-health-check" {
+  name               = "server-health-check"
   port               = 8000
-  request_path       = "/"
+  request_path       = "/server/healthcheck"
   check_interval_sec = 10
   timeout_sec        = 5
   healthy_threshold  = 2
