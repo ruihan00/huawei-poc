ea9d4f743957bfa7e89496e9a807660dfc8821e7
heyzec <61238538+heyzec@users.noreply.github.com>
2024-10-06 13:26:47 +0800

    script

--- infra/server-startup.sh
+++ infra/server-startup.sh
@@ -1,6 +1,20 @@
+# Install Nvidia drivers
+sudo /opt/deeplearning/install-driver.sh
+
+# Clone server code
 cd /
 git clone https://github.com/ruihan00/huawei-poc
+
+# Fix permissions issue
 sudo chgrp -R docker /huawei-poc
 sudo chmod g+w /huawei-poc
-docker run --pull always -d -p 8000:8080 -v /huawei-poc/server:/app hztiang/binacloud-huawei-poc-server
+
+# Start docker
+docker run        \
+    --pull always \
+    -d            \
+    -p 8000:8080  \
+    --gpus all     \
+    -v /huawei-poc/server:/app hztiang/binacloud-huawei-poc-server
+
 
