cb4218910958065bdb03716b401dde07ca9cef15
LimAiLin <limailin2009@gmail.com>
2024-10-09 23:08:53 +0800

    format

--- client/src/app/camera/page.jsx
+++ client/src/app/camera/page.jsx
@@ -2,20 +2,29 @@
 
 import Sender from "../ui/sender/sender";
 import { useState } from "react";
+import { Layout } from "antd";
+import PHeader from "../ui/layout/pheader"
+import PFooter from "../ui/layout/pfooter"
+
+const { Content } = Layout;
 
 const Page = () => {
   const [fps, setFps] = useState(1);
 
   return (
-    <div>
-      <label>FPS</label>
-      <input
-        type="number"
-        value={fps}
-        onChange={(e) => setFps(e.target.value)}
-      />
-      <Sender fps={fps} />
-    </div>
+    <Layout style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <PHeader title="Camera"/>
+      <Content style={{ backgroundColor: '#000', color: '#FFF', alignItems: 'center', justifyContent: 'center', padding: '24px' }}>
+        <label>FPS</label>
+        <input
+          type="number"
+          value={fps}
+          onChange={(e) => setFps(e.target.value)}
+        />
+        <Sender fps={fps} />
+      </Content>
+      <PFooter title="Camera"/>
+    </Layout>
   );
 };
 
