b3622dd9e7c9b6387f627ed4a55740554bb25069
heyzec <61238538+heyzec@users.noreply.github.com>
2024-10-09 23:02:43 +0800

    change startup script to docker compose

--- infra/server-startup.sh
+++ infra/server-startup.sh
@@ -10,12 +10,4 @@ sudo chgrp -R docker /huawei-poc
 sudo chmod g+w /huawei-poc
 
 # Start docker
-docker run                   \
-    --pull always            \
-    -d                       \
-    -p 8000:8080             \
-    --gpus all               \
-    --restart unless-stopped \
-    -v /huawei-poc/server:/app hztiang/binacloud-huawei-poc-server
-
-
+docker compose -f docker-compose.prod.yml up -d
