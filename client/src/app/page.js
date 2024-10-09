80e378da9cc7bbcdba49caa9396db339c14429bd
LimAiLin <limailin2009@gmail.com>
2024-10-09 23:08:21 +0800

    format

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -5,20 +5,31 @@ import { useState } from "react";
 
 import Sender from "./ui/sender/sender";
 import Receiver from "./ui/receiver/receiver";
+import PHeader from "./ui/layout/pheader"
+import PFooter from "./ui/layout/pfooter"
+import { Layout } from "antd";
+
+const { Content } = Layout;
 
 export default function Home() {
   const [fps, setFps] = useState(1);
 
   return (
-    <main className={styles.main}>
-      <label>FPS</label>
-      <input
-        type="number"
-        value={fps}
-        onChange={(e) => setFps(e.target.value)}
-      />
-      <Sender fps={fps} />
-      <Receiver />
-    </main>
+    <Layout style={{ display: 'flex', flexDirection: 'column' }}>
+      <PHeader title="SGTransaid"/>
+      <Content style={{ backgroundColor: '#000', color: '#FFF' }}>
+        <main className={styles.main}>
+          <label>FPS</label>
+          <input
+            type="number"
+            value={fps}
+            onChange={(e) => setFps(e.target.value)}
+          />
+          <Sender fps={fps} />
+          <Receiver />
+        </main>
+      </Content>
+      <PFooter title="SGTransaid"/>
+    </Layout>
   );
 }
