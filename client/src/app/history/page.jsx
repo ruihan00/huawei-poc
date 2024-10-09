e6b890ac72b0780d6bc1c9021f61b223d0382d84
LimAiLin <limailin2009@gmail.com>
2024-10-09 23:09:07 +0800

    ui

--- client/src/app/history/page.jsx
+++ client/src/app/history/page.jsx
@@ -1,36 +1,32 @@
 "use client";
 import { List, Splitter, Avatar, Layout } from "antd";
 import React, { useState, useEffect } from "react";
-import styles from "./styles.module.css";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { BASE_URL } from "../lib/api";
+import PHeader from "../ui/layout/pheader"
+import PFooter from "../ui/layout/pfooter"
 
-const { Header, Content, Footer } = Layout;
-
-// Fake data to mock events
-const fakeEvents = Array.from({ length: 50 }, (_, index) => ({
-  id: index + 1,
-  eventType: `Event Type ${index + 1}`,
-  time: `2024-10-08 12:0${index % 10}`,
-  description: `Description for event ${index + 1}`,
-  videoUrl: `https://www.example.com/videos/video${index + 1}.webm`
-}));
+const { Content } = Layout;
 
 const HistoryPage = () => {
   const [events, setEvents] = useState([]);
-  const [video, setVideo] = useState(null);
   const [selectedEvent, setSelectedEvent] = useState(null);
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
-    fetch(`${BASE_URL}/events`).then(data => {
-      console.log(data)
-    })
-    console.log("Loading initial data..."); // Debug log
+    console.log("Loading initial data...");
     loadMoreData();
   }, []);
   
+  const fetchEvents = async () => {
+    const response = await fetch(`${BASE_URL}/events`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  };
+
   // Simulate loading more data
   const loadMoreData = async () => {
     if (loading) {
@@ -38,7 +34,7 @@ const HistoryPage = () => {
     }
     setLoading(true);
     try {
-      const newEvents = fakeEvents.slice(events.length, events.length + 10); // Simulate loading next 10 events
+      const newEvents = await fetchEvents();
       if (newEvents.length === 0) {
         setHasMore(false);
       } else {
@@ -54,35 +50,53 @@ const HistoryPage = () => {
   // Show event details and set video
   const showEventDetails = (item) => {
     setSelectedEvent(item);
-    setVideo(item.videoUrl);
+  };
+
+  const getAvatarByType = (type) => {
+    switch (type) {
+      case 'Fall':
+        return <Avatar src={'/fall.png'}/>;
+      case 'Prolonged Time':
+        return <Avatar src={'/time.png'}/>;
+      case 'Mobility Aid':
+        return <Avatar src={'/mobaid.png'}/>;
+      default:
+        return <Avatar />
+    }
   };
 
   return (
     <Layout style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
-      <Header style={{ background: '#fff', padding: 0, height: '64px' }}>
-        <h1 style={{ margin: 0, padding: '0 16px' }}>Event History</h1>
-      </Header>
-      <Content style={{ padding: '50px' }}>
+      <PHeader title="Event History"/>
+      <Content style={{ padding: '50px', backgroundColor: '#FFF' }}>
         <div style={{
-          padding: 24,
           flex: 1,
           display: 'flex',
           flexDirection: 'column',
+          height: 'calc(100vh - 152px)' 
         }}>
           <Splitter 
             style={{
-              maxHeight: '70%',
+              height: '90%',
               boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
               overflow: 'hidden'
             }}
           >
             <Splitter.Panel defaultSize='40%' minSize='20%' maxSize='70%'>
-              <div style={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#f2eed7' }}>
-                {video ? (
+              <div style={{
+                height: '100%', 
+                display: 'flex', 
+                alignItems: 'center', 
+                justifyContent: 'center', 
+                backgroundColor: '#f2eed7', 
+                overflow: 'auto',
+                padding: '16px'
+              }}>
+                {selectedEvent ? (
                   <div>
                     <h1>ID: {selectedEvent.id}</h1>
-                    <video autoPlay={true} controls={true} style={{ maxWidth: '100%', maxHeight: 'calc(100% - 40px)' }}>
-                      <source src={video} type="video/webm" />
+                    <video key={selectedEvent.id} autoPlay={true} controls={true} style={{ maxWidth: '100%', maxHeight: 'calc(100% - 40px)' }}>
+                      <source src={selectedEvent.url} type="video/webm" />
                       Your browser does not support the video tag.
                     </video>
                   </div>
@@ -105,21 +119,31 @@ const HistoryPage = () => {
                   dataLength={events.length}
                   next={loadMoreData}
                   hasMore={hasMore}
-                  loader={<h4>Loading...</h4>}
                   scrollableTarget="scrollableDiv"
+                  inverse={true}
+                  style={{ display: 'flex', flexDirection: 'column-reverse' }} 
                 >
                   <List
-                    dataSource={events}
+                    dataSource={[...events].reverse()}
                     renderItem={(item) => (
                       <List.Item
                         key={item.id}
                         onClick={() => showEventDetails(item)}
-                        className={selectedEvent && selectedEvent.id === item.id ? styles.selectedItem : ""}
+                        style={{
+                          backgroundColor: selectedEvent && selectedEvent.id === item.id ? '#e6f7ff' : 'transparent',
+                          transition: 'background-color 0.3s',
+                          cursor: 'pointer',
+                        }}
                       >
                         <List.Item.Meta
-                          avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${item.id}`} />}
-                          title={item.eventType}
-                          description={<p>{item.time}</p>}
+                          avatar={getAvatarByType(item.type)}
+                          title={item.id}
+                          description={
+                            <div>
+                              <p>{item.type}</p>
+                              <p> {item.timestamp}</p>
+                            </div>
+                          }
                         />
                       </List.Item>
                     )}
@@ -130,10 +154,7 @@ const HistoryPage = () => {
           </Splitter>
         </div>
       </Content>
-      <Footer style={{ textAlign: 'center', height: '64px' }}>
-        {/* Add your footer content here */}
-        Event History ©{new Date().getFullYear()} Created by BinaCloud
-      </Footer>
+      <PFooter title="Event History"/>
     </Layout>
   );
 };
