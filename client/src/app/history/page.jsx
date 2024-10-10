926c61a9507c36d40820866a8930d649f3cc9911
LimAiLin <limailin2009@gmail.com>
2024-10-11 00:18:58 +0800

    hi

--- client/src/app/history/page.jsx
+++ client/src/app/history/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { Input, Select, List, Splitter, Avatar, Layout } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useMemo, Suspense } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { BASE_URL } from "../lib/api";
 import { FaPersonFalling } from "react-icons/fa6";
@@ -30,7 +30,7 @@ const HistoryPage = () => {
     };
   
     loadData();
-  }, []); // Empty dependency array means it runs once on mount
+  }, [events]);
   
   useEffect(() => {
     if (events.length > 0) {
@@ -157,118 +157,120 @@ const HistoryPage = () => {
             height: "calc(100vh - 152px)",
           }}
         >
-          <Splitter
-            style={{
-              height: "90%",
-              boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-              overflow: "hidden",
-            }}
-          >
-            <Splitter.Panel defaultSize="60%" minSize="20%" maxSize="70%">
-              <div
-                style={{
-                  height: "100%",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  backgroundColor: "#f2eed7",
-                  overflow: "auto",
-                  padding: "16px",
-                }}
-              >
-                {selectedEvent ? (
-                  <div>
-                    <video
-                      key={`${selectedEvent.id}-${new Date().getTime()}`}
-                      autoPlay={true}
-                      controls={true}
-                      style={{
-                        width: "640px",
-                        height: "480px",
-                      }}
-                    >
-                      <source src={selectedEvent.url} type="video/webm" />
-                      Your browser does not support the video tag.
-                    </video>
-                  </div>
-                ) : (
-                  <p>Select an event to view it!</p>
-                )}
-              </div>
-            </Splitter.Panel>
-            <Splitter.Panel style={{ height: "100%", overflow: "hidden" }}>
-              <div
-                id="scrollableDiv"
-                style={{
-                  height: "100%",
-                  overflow: "auto",
-                  padding: "16px",
-                  border: "1px solid rgba(140, 140, 140, 0.35)",
-                }}
-              >
-                <div style={{ padding: "16px", display: "flex", gap: "10px" }}>
-                  <Input
-                    placeholder="Search events"
-                    prefix={<SearchOutlined />}
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    style={{ flex: 1 }}
-                  />
-                  <Select
-                    style={{ width: 150 }}
-                    value={filterType}
-                    onChange={setFilterType}
-                  >
-                    {EVENT_TYPES.map((type) => (
-                      <Option key={type} value={type}>
-                        {type === "all"
-                          ? "All Types"
-                          : type.charAt(0).toUpperCase() + type.slice(1)}
-                      </Option>
-                    ))}
-                  </Select>
-                </div>
-                <InfiniteScroll
-                  dataLength={filteredEvents.length}
-                  next={loadMoreData}
-                  hasMore={hasMore}
-                  scrollableTarget="scrollableDiv"
-                  inverse={true}
-                  style={{ display: "flex", flexDirection: "column-reverse" }}
+          <Suspense fallback={<div>Loading...</div>}>
+            <Splitter
+              style={{
+                height: "90%",
+                boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+                overflow: "hidden",
+              }}
+            >
+              <Splitter.Panel defaultSize="60%" minSize="20%" maxSize="70%">
+                <div
+                  style={{
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: "#f2eed7",
+                    overflow: "auto",
+                    padding: "16px",
+                  }}
                 >
-                  <List
-                    dataSource={[...filteredEvents]}
-                    renderItem={(item) => (
-                      <List.Item
-                        key={item.id}
-                        onClick={() => showEventDetails(item)}
+                  {selectedEvent ? (
+                    <div>
+                      <video
+                        key={`${selectedEvent.id}-${new Date().getTime()}`}
+                        autoPlay={true}
+                        controls={true}
                         style={{
-                          backgroundColor:
-                            selectedEvent && selectedEvent.id === item.id
-                              ? "#e6f7ff"
-                              : "transparent",
-                          transition: "background-color 0.3s",
-                          cursor: "pointer",
-                          padding: "16px"
+                          width: "640px",
+                          height: "480px",
                         }}
                       >
-                        <List.Item.Meta
-                          avatar={getAvatarByType(item.type)}
-                          title={item.id}
-                          description={
-                            <div>
-                              <p>{item.type}</p>
-                              <p> {item.timestamp}</p>
-                            </div>
-                          }
-                        />
-                      </List.Item>
-                    )}
-                  />
-                </InfiniteScroll>
-              </div>
-            </Splitter.Panel>
-          </Splitter>
+                        <source src={selectedEvent.url} type="video/webm" />
+                        Your browser does not support the video tag.
+                      </video>
+                    </div>
+                  ) : (
+                    <p>Select an event to view it!</p>
+                  )}
+                </div>
+              </Splitter.Panel>
+              <Splitter.Panel style={{ height: "100%", overflow: "hidden" }}>
+                <div
+                  id="scrollableDiv"
+                  style={{
+                    height: "100%",
+                    overflow: "auto",
+                    padding: "16px",
+                    border: "1px solid rgba(140, 140, 140, 0.35)",
+                  }}
+                >
+                  <div style={{ padding: "16px", display: "flex", gap: "10px" }}>
+                    <Input
+                      placeholder="Search events"
+                      prefix={<SearchOutlined />}
+                      value={searchTerm}
+                      onChange={(e) => setSearchTerm(e.target.value)}
+                      style={{ flex: 1 }}
+                    />
+                    <Select
+                      style={{ width: 150 }}
+                      value={filterType}
+                      onChange={setFilterType}
+                    >
+                      {EVENT_TYPES.map((type) => (
+                        <Option key={type} value={type}>
+                          {type === "all"
+                            ? "All Types"
+                            : type.charAt(0).toUpperCase() + type.slice(1)}
+                        </Option>
+                      ))}
+                    </Select>
+                  </div>
+                  <InfiniteScroll
+                    dataLength={filteredEvents.length}
+                    next={loadMoreData}
+                    hasMore={hasMore}
+                    scrollableTarget="scrollableDiv"
+                    inverse={true}
+                    style={{ display: "flex", flexDirection: "column-reverse" }}
+                  >
+                    <List
+                      dataSource={[...filteredEvents]}
+                      renderItem={(item) => (
+                        <List.Item
+                          key={item.id}
+                          onClick={() => showEventDetails(item)}
+                          style={{
+                            backgroundColor:
+                              selectedEvent && selectedEvent.id === item.id
+                                ? "#e6f7ff"
+                                : "transparent",
+                            transition: "background-color 0.3s",
+                            cursor: "pointer",
+                            padding: "16px"
+                          }}
+                        >
+                          <List.Item.Meta
+                            avatar={getAvatarByType(item.type)}
+                            title={item.id}
+                            description={
+                              <div>
+                                <p>{item.type}</p>
+                                <p> {item.timestamp}</p>
+                              </div>
+                            }
+                          />
+                        </List.Item>
+                      )}
+                    />
+                  </InfiniteScroll>
+                </div>
+              </Splitter.Panel>
+            </Splitter>
+          </Suspense>
         </div>
       </Content>
       <PFooter title="Event History" />
